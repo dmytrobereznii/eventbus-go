ae33a3f5d1c195e17b68506454764878d5f827c8
dmytro <dima.berezniy.13@gmail.com>
2026-06-16 09:47:14 +0300

    feat: type-safe topics

--- bus.go
+++ bus.go
@@ -2,48 +2,59 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"sync"
 )
+
+type ChangeDelta struct {
+	NewDefaultRoute string
+}
+
+type RouteUpdate struct {
+	Added   []string
+	Removed []string
+}
 
 type Handler func(e any)
 
 type Bus struct {
 	mu     sync.RWMutex
-	topics map[string][]Handler
+	topics map[reflect.Type][]Handler
 }
 
 func NewBus() *Bus {
-	return &Bus{topics: make(map[string][]Handler)}
+	return &Bus{topics: make(map[reflect.Type][]Handler)}
 }
 
-func (b *Bus) Subscribe(topic string, f Handler) {
+func Subscribe[T any](b *Bus, h Handler) {
 	b.mu.Lock()
-	b.topics[topic] = append(b.topics[topic], f)
+	b.topics[reflect.TypeFor[T]()] = append(b.topics[reflect.TypeFor[T]()], h)
 	b.mu.Unlock()
 }
 
-func (b *Bus) Publish(topic string, msg any) {
+func Publish[T any](b *Bus, e T) {
 	b.mu.RLock()
-	subscribers, ok := b.topics[topic]
+	subscribers, ok := b.topics[reflect.TypeFor[T]()]
 	b.mu.RUnlock()
 	if !ok {
 		return
 	}
 
 	for _, handler := range subscribers {
-		handler(msg) // slow handler delays whole flow
+		handler(e) // slow handler delays whole flow
 	}
 }
 
 func main() {
 	bus := NewBus()
 
-	bus.Subscribe("user.created", func(msg any) {
-		fmt.Println("user.created - msg:", msg)
+	Subscribe[ChangeDelta](bus, func(e any) {
+		fmt.Printf("%s\n", e)
 	})
-	bus.Subscribe("user.updated", func(msg any) {
-		fmt.Println("user.updated - msg:", msg)
+	Subscribe[RouteUpdate](bus, func(e any) {
+		fmt.Printf("%s\n", e)
 	})
 
-	bus.Publish("user.created", "John Doe")
+	Publish[ChangeDelta](bus, ChangeDelta{NewDefaultRoute: "192.168.1.1"})
+	Publish[RouteUpdate](bus, RouteUpdate{Added: []string{"10.0.0.0/8"}})
 }
